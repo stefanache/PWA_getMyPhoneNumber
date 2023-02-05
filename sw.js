ac296f9d7db976da7357b64e841790afa2bee29f
Stefanache Ion <ion.stefanache@yahoo.com>
2023-02-05 19:11:22 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
